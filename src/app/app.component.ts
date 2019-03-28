ad977eeb3ece017765c70d451b6283dec7bec8a9
Pant3r <omar@gusacapital.com>
2019-03-28 12:57:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	providers: [WebsocketService]
 })
 export class AppComponent {
-  title = 'socket-angular';
+	title = 'socket-angular';
+
+	constructor(
+		private WebsocketService: WebsocketService
+	){}
+
+	ngOnInit(){
+		//this.socket()
+	}
+
+	socket(){
+		this.WebsocketService.listen('test').subscribe((data) => {
+			console.log(data)
+		})
+	}
 }
